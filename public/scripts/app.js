ba5380ee005efddbad42fe5b28f3d7f51f8852b6
Rondale W. Davis <rdavis2005@msn.com>
2017-11-22 00:55:12 +0000

    app.js changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,54 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 2'
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Rondale Davis'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 31'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Durham'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
